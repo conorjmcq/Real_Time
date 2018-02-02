cb0e1dce3a9525a825353f35fd0851b4f848d126
cmcq-college <ConorJmcs@hotmail.com>
2018-02-02 15:31:17 +0000

    Added comments on glClearColor()

--- Assignment_1/main.cpp
+++ Assignment_1/main.cpp
@@ -6,7 +6,9 @@ void renderScene(void)
 {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0, 0.0, 0.0, 1.0);//clear red
+	glClearColor(0.0, 1.0, 0.0, 1.0);//clear red
+
+	//glClearColor('red', 'green', 'blue', 'alpha') => Where alpha is the opacity value.
 
 	glutSwapBuffers();
 }
@@ -19,6 +21,16 @@ int main(int argc, char **argv)
 	glutInitWindowPosition(500, 500);//optional
 	glutInitWindowSize(800, 600); //optional
 	glutCreateWindow("OpenGL First Window");
+
+	glewInit();
+	if (glewIsSupported("GL_VERSION_4_5"))
+	{
+		std::cout << " GLEW Version is 4.5\n ";
+	}
+	else
+	{
+		std::cout << "GLEW 4.5 not supported\n ";
+	}
 
 	glEnable(GL_DEPTH_TEST);
 
