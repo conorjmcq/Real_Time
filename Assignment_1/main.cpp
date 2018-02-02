a0ea8aa07775f9e9fd7bb0cefd0f88b6e29eba8e
cmcq-college <ConorJmcs@hotmail.com>
2018-02-02 15:06:06 +0000

    Added window with clear red coloring.

--- Assignment_1/main.cpp
+++ Assignment_1/main.cpp
@@ -1,4 +1,30 @@
+#include "Internal_Dependencies\glew\glew.h"
+#include "Internal_Dependencies\freeglut\freeglut.h"
+#include <iostream>
+
+void renderScene(void)
+{
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(1.0, 0.0, 0.0, 1.0);//clear red
+
+	glutSwapBuffers();
+}
+
 int main(int argc, char **argv)
 {
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(500, 500);//optional
+	glutInitWindowSize(800, 600); //optional
+	glutCreateWindow("OpenGL First Window");
+
+	glEnable(GL_DEPTH_TEST);
+
+	glutDisplayFunc(renderScene);
+
+	glutMainLoop();
+
 	return 0;
 }
