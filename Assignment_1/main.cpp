a7f8de569ddc505bdf5e9d3e8209ceabb35d4ebd
Conor McQuillan <ConorJmcs@hotmail.com>
2018-02-07 18:44:20 +0000

    Updated master branch to reflect changes on mac.

--- Assignment_1/main.cpp
+++ Assignment_1/main.cpp
@@ -1,42 +1,105 @@
-#include "Internal_Dependencies\glew\glew.h"
-#include "Internal_Dependencies\freeglut\freeglut.h"
-#include <iostream>
+#ifdef __APPLE__
+#	include <GL/glew.h>
+#	include <GLFW/glfw3.h> // GLFW helper library
+#	include <stdio.h>
+#else
+#	include "Internal_Dependencies\glew\glew.h"
+#	include "Internal_Dependencies\glfw\glfw3.h"
+#	include <iostream>
+#endif
+const char* vertex_shader =
+"#version 400\n"
+"in vec3 vp;"
+"void main() {"
+"  gl_Position = vec4(vp, 1.0);"
+"}";
 
-void renderScene(void)
-{
+const char* fragment_shader =
+"#version 400\n"
+"out vec4 frag_colour;"
+"void main() {"
+"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+"}";
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0.0, 1.0, 0.0, 1.0);//clear red
+int main() {
+	// start GL context and O/S window using the GLFW helper library
+	if (!glfwInit()) {
+		//printf(stderr, "ERROR: could not start GLFW3\n");
+		return 1;
+	}
 
-	//glClearColor('red', 'green', 'blue', 'alpha') => Where alpha is the opacity value.
+	// uncomment these lines if on Apple OS X
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	glutSwapBuffers();
-}
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+	if (!window) {
+		//fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+		glfwTerminate();
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
 
-int main(int argc, char **argv)
-{
-
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(500, 500);//optional
-	glutInitWindowSize(800, 600); //optional
-	glutCreateWindow("OpenGL First Window");
-
+	// start GLEW extension handler
+	glewExperimental = GL_TRUE;
 	glewInit();
-	if (glewIsSupported("GL_VERSION_4_5"))
-	{
-		std::cout << " GLEW Version is 4.5\n ";
+
+	// get version info
+	const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+	const GLubyte* version = glGetString(GL_VERSION); // version as a string
+	//
+
+	// tell GL to only draw onto a pixel if the shape is closer to the viewer
+	glEnable(GL_DEPTH_TEST); // enable depth-testing
+	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
+						  /* OTHER STUFF GOES HERE NEXT */
+	float points[] = {
+		0.0f,  0.5f,  0.0f,
+		0.5f, -0.5f,  0.0f,
+		-0.5f, -0.5f,  0.0f
+	};
+
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	GLuint shader_programme = glCreateProgram();
+	glAttachShader(shader_programme, fs);
+	glAttachShader(shader_programme, vs);
+	glLinkProgram(shader_programme);
+
+	while (!glfwWindowShouldClose(window)) {
+		// wipe the drawing surface clear
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(shader_programme);
+		glBindVertexArray(vao);
+		// draw points 0-3 from the currently bound VAO with current in-use shader
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// update other events like input handling 
+		glfwPollEvents();
+		// put the stuff we've been drawing onto the display
+		glfwSwapBuffers(window);
 	}
-	else
-	{
-		std::cout << "GLEW 4.5 not supported\n ";
-	}
 
-	glEnable(GL_DEPTH_TEST);
-
-	glutDisplayFunc(renderScene);
-
-	glutMainLoop();
-
+	// close GL context and any other GLFW resources
+	glfwTerminate();
 	return 0;
 }
