f615323d37d7dd2f55daca23119b7186dd448be6
Conor McQuillan <ConorJmcs@hotmail.com>
2018-02-13 19:57:14 +0000

    Updated entire project, now loads cat.dae and rotates. - cmcq

--- Assignment_1/main.cpp
+++ Assignment_1/main.cpp
@@ -3,103 +3,374 @@
 #	include <GLFW/glfw3.h> // GLFW helper library
 #	include <stdio.h>
 #else
-#	include "Internal_Dependencies\glew\glew.h"
-#	include "Internal_Dependencies\glfw\glfw3.h"
+#	define _CRT_SECURE_NO_DEPRECATE
+#	define STB_IMAGE_IMPLEMENTATION
+#	include "Internal_Dependencies\glew\include\glew.h"
+#	include "Internal_Dependencies\glfw\include\glfw3.h"
+#	include "Internal_Dependencies\glm\glm\glm.hpp"
+#	include "Internal_Dependencies\freeglut\include\GL\freeglut.h"
+#	include "Internal_Dependencies\glm\glm\gtc\matrix_transform.hpp"
+#	include "Internal_Dependencies\glm\glm\gtx\matrix_decompose.hpp"
+#	include "Internal_Dependencies\assimp\include\assimp\cimport.h" // C importer
+#	include "Internal_Dependencies\assimp\include\assimp\scene.h" // collects data
+#	include "Internal_Dependencies\assimp\include\assimp\postprocess.h" // various extra operations
+#	include "Internal_Dependencies\assimp\include\assimp\importerdesc.h"
+#	include "maths_funcs.h"
+#	include "teapot.h"
 #	include <iostream>
+#	include <stdio.h>
+#	include <vector>
+#	include <fstream>
+#	include <iostream>
+#	include <sstream>
+#	include <math.h>
 #endif
-const char* vertex_shader =
-"#version 400\n"
-"in vec3 vp;"
-"void main() {"
-"  gl_Position = vec4(vp, 1.0);"
-"}";
 
-const char* fragment_shader =
-"#version 400\n"
-"out vec4 frag_colour;"
-"void main() {"
-"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
-"}";
+using std::vector;
+using std::ifstream;
+using std::string;
+using std::istringstream;
 
-int main() {
-	// start GL context and O/S window using the GLFW helper library
-	if (!glfwInit()) {
-		//printf(stderr, "ERROR: could not start GLFW3\n");
-		return 1;
+//Some Windows Headers (For Time, IO, etc.)
+#include <windows.h>
+#include <mmsystem.h>
+
+
+/*----------------------------------------------------------------------------
+MESH TO LOAD
+----------------------------------------------------------------------------*/
+// this mesh is a dae file format but you should be able to use any other format too, obj is typically what is used
+// put the mesh in your project directory, or provide a filepath for it here
+#define MESH_NAME "cat.dae"
+/*----------------------------------------------------------------------------
+----------------------------------------------------------------------------*/
+
+std::vector<float> g_vp, g_vn, g_vt;
+int g_point_count = 0;
+
+
+// Macro for indexing vertex buffer
+#define BUFFER_OFFSET(i) ((char *)NULL + (i))
+
+using namespace std;
+GLuint shaderProgramID;
+
+
+unsigned int mesh_vao = 0;
+int width = 800;
+int height = 600;
+
+GLuint loc1, loc2, loc3;
+GLfloat rotate_y = 0.0f;
+
+
+#pragma region MESH LOADING
+/*----------------------------------------------------------------------------
+MESH LOADING FUNCTION
+----------------------------------------------------------------------------*/
+
+bool load_mesh(const char* file_name) {
+	const aiScene* scene = aiImportFile(file_name, aiProcess_Triangulate); // TRIANGLES!
+	fprintf(stderr, "ERROR: reading mesh %s\n", file_name);
+	if (!scene) {
+		fprintf(stderr, "ERROR: reading mesh %s\n", file_name);
+		return false;
+	}
+	printf("  %i animations\n", scene->mNumAnimations);
+	printf("  %i cameras\n", scene->mNumCameras);
+	printf("  %i lights\n", scene->mNumLights);
+	printf("  %i materials\n", scene->mNumMaterials);
+	printf("  %i meshes\n", scene->mNumMeshes);
+	printf("  %i textures\n", scene->mNumTextures);
+
+	for (unsigned int m_i = 0; m_i < scene->mNumMeshes; m_i++) {
+		const aiMesh* mesh = scene->mMeshes[m_i];
+		printf("    %i vertices in mesh\n", mesh->mNumVertices);
+		g_point_count = mesh->mNumVertices;
+		for (unsigned int v_i = 0; v_i < mesh->mNumVertices; v_i++) {
+			if (mesh->HasPositions()) {
+				const aiVector3D* vp = &(mesh->mVertices[v_i]);
+				//printf ("      vp %i (%f,%f,%f)\n", v_i, vp->x, vp->y, vp->z);
+				g_vp.push_back(vp->x);
+				g_vp.push_back(vp->y);
+				g_vp.push_back(vp->z);
+			}
+			if (mesh->HasNormals()) {
+				const aiVector3D* vn = &(mesh->mNormals[v_i]);
+				//printf ("      vn %i (%f,%f,%f)\n", v_i, vn->x, vn->y, vn->z);
+				g_vn.push_back(vn->x);
+				g_vn.push_back(vn->y);
+				g_vn.push_back(vn->z);
+			}
+			if (mesh->HasTextureCoords(0)) {
+				const aiVector3D* vt = &(mesh->mTextureCoords[0][v_i]);
+				//printf ("      vt %i (%f,%f)\n", v_i, vt->x, vt->y);
+				g_vt.push_back(vt->x);
+				g_vt.push_back(vt->y);
+			}
+			if (mesh->HasTangentsAndBitangents()) {
+				// NB: could store/print tangents here
+			}
+		}
 	}
 
-	// uncomment these lines if on Apple OS X
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	aiReleaseImport(scene);
+	return true;
+}
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
-	if (!window) {
-		//fprintf(stderr, "ERROR: could not open window with GLFW3\n");
-		glfwTerminate();
-		return 1;
+#pragma endregion MESH LOADING
+
+// Shader Functions- click on + to expand
+#pragma region SHADER_FUNCTIONS
+
+// Create a NULL-terminated string by reading the provided file
+char* readShaderSource(const char* shaderFile) {
+	FILE* fp = fopen(shaderFile, "rb"); //!->Why does binary flag "RB" work and not "R"... wierd msvc thing?
+
+	if (fp == NULL) { return NULL; }
+
+	fseek(fp, 0L, SEEK_END);
+	long size = ftell(fp);
+
+	fseek(fp, 0L, SEEK_SET);
+	char* buf = new char[size + 1];
+	fread(buf, 1, size, fp);
+	buf[size] = '\0';
+
+	fclose(fp);
+
+	return buf;
+}
+
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+	// create a shader object
+	GLuint ShaderObj = glCreateShader(ShaderType);
+
+	if (ShaderObj == 0) {
+		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+		exit(0);
 	}
-	glfwMakeContextCurrent(window);
+	const char* pShaderSource = readShaderSource(pShaderText);
 
-	// start GLEW extension handler
-	glewExperimental = GL_TRUE;
-	glewInit();
+	// Bind the source code to the shader, this happens before compilation
+	glShaderSource(ShaderObj, 1, (const GLchar**)&pShaderSource, NULL);
+	// compile the shader and check for errors
+	glCompileShader(ShaderObj);
+	GLint success;
+	// check for shader related errors using glGetShaderiv
+	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		GLchar InfoLog[1024];
+		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+		exit(1);
+	}
+	// Attach the compiled shader object to the program object
+	glAttachShader(ShaderProgram, ShaderObj);
+}
 
-	// get version info
-	const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-	const GLubyte* version = glGetString(GL_VERSION); // version as a string
-	//
+GLuint CompileShaders()
+{
+	//Start the process of setting up our shaders by creating a program ID
+	//Note: we will link all the shaders together into this ID
+	shaderProgramID = glCreateProgram();
+	if (shaderProgramID == 0) {
+		fprintf(stderr, "Error creating shader program\n");
+		exit(1);
+	}
+
+	// Create two shader objects, one for the vertex, and one for the fragment shader
+	AddShader(shaderProgramID, "../Shaders/simpleVertexShader.txt", GL_VERTEX_SHADER);
+	AddShader(shaderProgramID, "../Shaders/simpleFragmentShader.txt", GL_FRAGMENT_SHADER);
+
+	GLint Success = 0;
+	GLchar ErrorLog[1024] = { 0 };
+	// After compiling all shader objects and attaching them to the program, we can finally link it
+	glLinkProgram(shaderProgramID);
+	// check for program related errors using glGetProgramiv
+	glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &Success);
+	if (Success == 0) {
+		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+		exit(1);
+	}
+
+	// program has been successfully linked but needs to be validated to check whether the program can execute given the current pipeline state
+	glValidateProgram(shaderProgramID);
+	// check for program related errors using glGetProgramiv
+	glGetProgramiv(shaderProgramID, GL_VALIDATE_STATUS, &Success);
+	if (!Success) {
+		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+		exit(1);
+	}
+	// Finally, use the linked shader program
+	// Note: this program will stay in effect for all draw calls until you replace it with another or explicitly disable its use
+	glUseProgram(shaderProgramID);
+	return shaderProgramID;
+}
+#pragma endregion SHADER_FUNCTIONS
+
+// VBO Functions - click on + to expand
+#pragma region VBO_FUNCTIONS
+
+void generateObjectBufferMesh() {
+	/*----------------------------------------------------------------------------
+	LOAD MESH HERE AND COPY INTO BUFFERS
+	----------------------------------------------------------------------------*/
+
+	//Note: you may get an error "vector subscript out of range" if you are using this code for a mesh that doesnt have positions and normals
+	//Might be an idea to do a check for that before generating and binding the buffer.
+
+	load_mesh(MESH_NAME);
+	unsigned int vp_vbo = 0;
+	loc1 = glGetAttribLocation(shaderProgramID, "vertex_position");
+	loc2 = glGetAttribLocation(shaderProgramID, "vertex_normal");
+	loc3 = glGetAttribLocation(shaderProgramID, "vertex_texture");
+
+	glGenBuffers(1, &vp_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vp_vbo);
+	glBufferData(GL_ARRAY_BUFFER, g_point_count * 3 * sizeof(float), &g_vp[0], GL_STATIC_DRAW);
+	unsigned int vn_vbo = 0;
+	glGenBuffers(1, &vn_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vn_vbo);
+	glBufferData(GL_ARRAY_BUFFER, g_point_count * 3 * sizeof(float), &g_vn[0], GL_STATIC_DRAW);
+
+	//	This is for texture coordinates which you don't currently need, so I have commented it out
+	//	unsigned int vt_vbo = 0;
+	//	glGenBuffers (1, &vt_vbo);
+	//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
+	//	glBufferData (GL_ARRAY_BUFFER, g_point_count * 2 * sizeof (float), &g_vt[0], GL_STATIC_DRAW);
+
+	unsigned int vao = 0;
+	glBindVertexArray(vao);
+
+	glEnableVertexAttribArray(loc1);
+	glBindBuffer(GL_ARRAY_BUFFER, vp_vbo);
+	glVertexAttribPointer(loc1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	glEnableVertexAttribArray(loc2);
+	glBindBuffer(GL_ARRAY_BUFFER, vn_vbo);
+	glVertexAttribPointer(loc2, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	//	This is for texture coordinates which you don't currently need, so I have commented it out
+	//	glEnableVertexAttribArray (loc3);
+	//	glBindBuffer (GL_ARRAY_BUFFER, vt_vbo);
+	//	glVertexAttribPointer (loc3, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+
+}
+
+
+#pragma endregion VBO_FUNCTIONS
+
+
+void display() {
 
 	// tell GL to only draw onto a pixel if the shape is closer to the viewer
 	glEnable(GL_DEPTH_TEST); // enable depth-testing
 	glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(shaderProgramID);
 
-						  /* OTHER STUFF GOES HERE NEXT */
-	float points[] = {
-		0.0f,  0.5f,  0.0f,
-		0.5f, -0.5f,  0.0f,
-		-0.5f, -0.5f,  0.0f
-	};
 
-	GLuint vbo = 0;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+	//Declare your uniform variables that will be used in your shader
+	int matrix_location = glGetUniformLocation(shaderProgramID, "model");
+	int view_mat_location = glGetUniformLocation(shaderProgramID, "view");
+	int proj_mat_location = glGetUniformLocation(shaderProgramID, "proj");
 
-	GLuint vao = 0;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	// Root of the Hierarchy
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 persp_proj = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 100.0f);
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::scale(model, glm::vec3(0.005f, 0.005f, 0.005f));
+	model = glm::rotate(model, 3.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+	model = glm::rotate(model, 1.2f, glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::rotate(model, rotate_y, glm::vec3(0.0f, 0.0f, 1.0f));
+	view = translate(view, glm::vec3(0.0, 0.0, -5.0f));
 
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vs, 1, &vertex_shader, NULL);
-	glCompileShader(vs);
-	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fs, 1, &fragment_shader, NULL);
-	glCompileShader(fs);
+	// update uniforms & draw
+	glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, &persp_proj[0][0]);
+	glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, &view[0][0]);
+	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, &model[0][0]);
 
-	GLuint shader_programme = glCreateProgram();
-	glAttachShader(shader_programme, fs);
-	glAttachShader(shader_programme, vs);
-	glLinkProgram(shader_programme);
+	glDrawArrays(GL_TRIANGLES, 0, g_point_count);
+	glutSwapBuffers();
+}
 
-	while (!glfwWindowShouldClose(window)) {
-		// wipe the drawing surface clear
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glUseProgram(shader_programme);
-		glBindVertexArray(vao);
-		// draw points 0-3 from the currently bound VAO with current in-use shader
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// update other events like input handling 
-		glfwPollEvents();
-		// put the stuff we've been drawing onto the display
-		glfwSwapBuffers(window);
+
+void updateScene() {
+
+	// Placeholder code, if you want to work with framerate
+	// Wait until at least 16ms passed since start of last frame (Effectively caps framerate at ~60fps)
+	static DWORD  last_time = 0;
+	DWORD  curr_time = timeGetTime();
+	float  delta = (curr_time - last_time) * 0.001f;
+	if (delta > 0.0003f)
+		delta = 0.0003f;
+	last_time = curr_time;
+
+	// rotate the model slowly around the y axis
+	rotate_y += 0.002f;
+	// Draw the next frame
+	glutPostRedisplay();
+}
+
+
+void init()
+{
+	// Set up the shaders
+	GLuint shaderProgramID = CompileShaders();
+	// load mesh into a vertex buffer array
+	generateObjectBufferMesh();
+
+}
+
+// Placeholder code for the keypress
+void keypress(unsigned char key, int x, int y) {
+
+	if (key == 'x') {
+		//Translate the base, etc.
 	}
 
-	// close GL context and any other GLFW resources
-	glfwTerminate();
+}
+
+int main(int argc, char** argv) {
+
+	// Set up the window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("Hello Triangle");
+
+	// Tell glut where the display function is
+	glutDisplayFunc(display);
+	glutIdleFunc(updateScene);
+	glutKeyboardFunc(keypress);
+
+	// A call to glewInit() must be done after glut is initialized!
+	GLenum res = glewInit();
+	// Check for any errors
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+	// Set up your objects and shaders
+	init();
+	
+	// Begin infinite event loop
+	glutMainLoop();
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
